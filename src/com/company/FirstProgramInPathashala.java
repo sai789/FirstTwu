1c181f1eb43d27ecbde174749cb06ccd93edb6af
saikumar <vantakusaikumar562@gmail.com>
2017-07-16 14:34:00 +0530

    all files were added

---
+++ src/com/company/FirstProgramInPathashala.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by saikv on 7/14/17.
+ */
+public class FirstProgramInPathashala {
+    public static void main(String a[]){
+        System.out.println("Hai! This is Hello World");
+    }
+}
